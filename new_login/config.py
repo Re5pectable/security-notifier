72bc11885895f4ab51f186994ad8f99492b1bcff
Басистов Глеб <basistovga@cash-u.com>
2024-01-17 19:37:19 +0700

    fix

--- new_login/config.py
+++ new_login/config.py
@@ -4,3 +4,11 @@ SSH_PORT: int = config_parser.getint('Server', 'SSH_PORT')
 SERVER_NAME: str = config_parser.get('Server', 'NAME')
 TG_TOKEN: str = config_parser.get('Telegram', 'TOKEN')
 TG_CHAT: int = config_parser.getint('Telegram', 'CHAT_ID')
+
+if not all([
+    SSH_PORT,
+    SERVER_NAME,
+    TG_TOKEN,
+    TG_CHAT
+]):
+    raise ValueError('Cannot extract configuration')
--- new_login/notify.py
+++ new_login/notify.py
@@ -7,14 +7,13 @@ from .config import SERVER_NAME, TG_CHAT, TG_TOKEN
 
 message_template = """
 User *{username}* logged in to *{server_name}*
-
-*{time}*
+`{time}`
 """
 
 def notify():
     text = message_template.format(
         username=os.getenv('USER'),
-        server_name=datetime.now().strftime('%H:%M:%S %Y-%m-%d'),
-        time=SERVER_NAME
+        server_name=SERVER_NAME,
+        time=datetime.now().strftime('%H:%M:%S %Y-%m-%d')
     )
     Telegram(TG_TOKEN).send_text(TG_CHAT, text)
