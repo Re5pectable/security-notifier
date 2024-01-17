05706ced16fd169fd9fc10ce1e23ea20ee995db1
Басистов Глеб <basistovga@cash-u.com>
2024-01-17 21:44:45 +0700

    fix

---
+++ Makefile
@@ -0,0 +1,2 @@
+new-login:
+	@cd /Users/gleb/Documents/Projects/Python/security && python3.11 -m new_login
--- new_login/__main__.py
+++ new_login/__main__.py
@@ -1,4 +1,4 @@
-from .notify import notify
+from new_login.new_login import notify
 
 if __name__ == '__main__':
     notify()
