4dea5f0a97cb3395fac26d60ea1720b7a84fb1a5
Басистов Глеб <basistovga@cash-u.com>
2024-01-17 22:54:07 +0700

    fix

--- no_access_check/config.py
+++
@@ -1,5 +0,0 @@
-from src.configuration import config_parser
-
-SSH_PORT: int = config_parser.getint('Server', 'SSH_PORT')
-TG_CHAT: int = config_parser.getint('Telegram', 'TG_CHAT')
-WIREGUARD_SUBNET: str = config_parser.get('Server', 'WIREGUARD_SUBNET')
--- no_access_check/readme.md
+++
@@ -1,7 +0,0 @@
-# Usage
-
-With cron:
-
-```bash
-*/15 * * * * cd <path_to_parent_repo> && /usr/bin/python3 security-notifier
-```
